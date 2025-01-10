4bc938b55bcb4d45e35cef7b15e7b44c7df14151
amphi2024 <amphiprioninae2024@gmail.com>
2025-01-10 10:19:19 +0900

    commit

--- routes/servers.js
+++ routes/servers.js
@@ -3,24 +3,31 @@ const router = express.Router();
 const fs = require('fs');
 const path = require("node:path");
 
+router.get('/latest', (req, res) => {
+
+    const filename = "server-1.0.0.jar";
+    streamFile(res,filename, path.join(__dirname, 'files', "servers", filename));
+
+});
+
 router.get('/:filename', (req, res) => {
 
         const filename = req.params.filename;
 
-        const filePath = path.join(__dirname, 'files', "servers" , filename);
-
-        console.log('Resolved file path:', filePath);
-
-        // 파일 스트리밍 다운로드
-        const readStream = fs.createReadStream(filePath);
-        res.setHeader('Content-Disposition', `attachment; filename=${filename}`);
-        readStream.pipe(res);
-
-        readStream.on('error', (err) => {
-            console.error('File streaming error:', err);
-            res.status(500).send('Error streaming the file');
-        });
+        streamFile(res, filename, path.join(__dirname, 'files', "servers" , filename));
 
 });
+
+function streamFile(res, filename, filePath) {
+
+    const readStream = fs.createReadStream(filePath);
+    res.setHeader('Content-Disposition', `attachment; filename=${filename}`);
+    readStream.pipe(res);
+
+    readStream.on('error', (err) => {
+        console.error('File streaming error:', err);
+        res.status(500).send('Error streaming the file');
+    });
+}
 
 module.exports = router;
