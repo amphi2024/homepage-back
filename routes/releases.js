a5f382cf36995d277fb871e9a1592c14e6284139
Amphi <amphiprioninae2024@gmail.com>
2025-01-13 12:21:11 +0900

    notes app linux release

--- routes/releases.js
+++ routes/releases.js
@@ -3,13 +3,22 @@ const router = express.Router();
 const path = require("path");
 const streamFile = require("./streamFile");
 
-router.get('/notes/:os/:arch/:type/:filename', (req, res) => {
+router.get('/:name/:os/:filename', (req, res) => {
 
+    const name = req.params.name;
+    const os = req.params.os;
+    const filename = req.params.filename;
+    streamFile(res,filename, path.join(__dirname, 'files', "releases", name, os, filename));
+
+});
+
+router.get('/:name/:os/:arch/:type/:filename', (req, res) => {
+    const name = req.params.name;
     const os = req.params.os;
     const arch = req.params.arch;
     const type = req.params.type;
     const filename = req.params.filename;
-    streamFile(res,filename, path.join(__dirname, 'files', "releases", "notes", os, arch , type, filename));
+    streamFile(res,filename, path.join(__dirname, 'files', "releases", name, os, arch , type, filename));
 
 });
 module.exports = router;
