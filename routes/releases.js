3a0e0f3cd94ac57140310e5fbe8eb005c85fd34c
Amphi <amphiprioninae2024@gmail.com>
2025-02-18 17:05:51 +0900

    changed endpoint design

--- routes/releases.js
+++ routes/releases.js
@@ -3,33 +3,11 @@ const router = express.Router();
 const path = require("path");
 const streamFile = require("./streamFile");
 
-router.get('/:name/:os/:filename', (req, res) => {
+router.get('/:name/:filename', (req, res) => {
 
     const name = req.params.name;
-    const os = req.params.os;
     const filename = req.params.filename;
-    streamFile(res,filename, path.join(__dirname, 'files', "releases", name, os, filename));
-
-});
-
-router.get('/:name/:os/:arch/:type/:filename', (req, res) => {
-    const name = req.params.name;
-    const os = req.params.os;
-    const arch = req.params.arch;
-    const type = req.params.type;
-    const filename = req.params.filename;
-    streamFile(res,filename, path.join(__dirname, 'files', "releases", name, os, arch , type, filename));
-
-});
-
-router.get('/:name/:os/:arch/:type/:version/:filename', (req, res) => {
-    const name = req.params.name;
-    const os = req.params.os;
-    const arch = req.params.arch;
-    const type = req.params.type;
-    const version = req.params.version;
-    const filename = req.params.filename;
-    streamFile(res,filename, path.join(__dirname, 'files', "releases", name, os, arch , type, version , filename));
+    streamFile(res,filename, path.join(__dirname, 'files', "releases", name, filename));
 
 });
 
